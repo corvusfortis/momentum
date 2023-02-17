d1a7c09ac88b8ef794dab33c8ba9f083ec9ca4ae
corvusfortis <denisavidzba@mail.ru>
2023-02-17 14:26:22 +0300

    feat: add weather

--- js/script.js
+++ js/script.js
@@ -119,3 +119,38 @@
 
 })();
 
+(function weather(){
+    const weatherIcon = document.querySelector('.weather-icon');
+    const temperature = document.querySelector('.temperature');
+    const weatherDescription = document.querySelector('.weather-description');
+    const city = document.querySelector('.city');
+
+    async function getWeather() {  
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=ru&appid=08f2a575dda978b9c539199e54df03b0&units=metric`;
+        const res = await fetch(url);
+        const data = await res.json(); 
+        console.log(data.weather[0].id, data.weather[0].description, data.main.temp);
+
+        weatherIcon.className = 'weather-icon owf';
+        weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+        temperature.textContent = `${Math.floor(data.main.temp)}°C`;
+        weatherDescription.textContent = data.weather[0].description;
+
+        localStorage.setItem('city', city.value);
+      }
+      getWeather()
+
+     window.addEventListener('DOMContentLoaded', function(){
+         city.value = localStorage.getItem('city');
+         getWeather();
+     });
+     
+     city.addEventListener('blur', getWeather);
+     city.addEventListener('keypress', function(event){
+         if(event.key === 'Enter'){
+             getWeather();
+         }
+     })
+
+     
+})();
